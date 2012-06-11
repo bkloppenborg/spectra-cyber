f644baa9cf34280632298c164c1a2c8a7fccc6c1
Brian Kloppenborg <bkloppenborg@gmail.com>
2012-06-10 20:31:27 -0600

    Added copyright and licensing text.

--- SpectraCyber/ctrlPlotter.Designer.cs
+++ SpectraCyber/ctrlPlotter.Designer.cs
@@ -1,3 +1,30 @@
+/* 
+ * Copyright (c) 2007 Brian Kloppenborg
+ *
+ * If you use this software as part of a scientific publication, please cite as:
+ *
+ * Kloppenborg, B. (2012), "SpectraCyber Control Software"
+ * (Version X). Available from  <https://github.com/bkloppenborg/spectra-cyber>.
+ *
+ * This file is part of the SpectraCyber Control Software (SCCS).  
+ *
+ * The SpectraCyber was developed by Dr. John Bernard and gifted to
+ * Radio Astronomy Supplies (RAS).  "SpectraCyber" is used with 
+ * permission from RAS.
+ * 
+ * SCCS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation version 3.
+ * 
+ * SCCS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with SCCS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace SpectraCyber
 {
     partial class ctrlPlotter
